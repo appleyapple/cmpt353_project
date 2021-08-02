c1183e2d84a816d31303f2154059d1e0038939f5
Henry Yip <hya59@sfu.ca>
2021-08-02 14:00:03 -0700

    data loading, cleaning, smoothing, added TODO

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # cmpt353_project
+
+## Interpreting the Data
+phone is attached to the ankle facing outwards.
+x: leg moving forward/backwarad
+y: leg moving up/down
+z: leg moving side to side
+
+## TODO
+-get datasets: normal, limp, dragging feet
+-smooth/clean data (butterworth filter)
+-get frequency of steps (FFT, fourier transform)
+-transform acceleration to velocity to position (Δv = a⋅Δt and Δp = v⋅Δt.)
+-check normality/variance 
+-statistical test to see if datasets differ (anova, u-test, t-test)
+-compare different steps
+-train ml to detect injury
+
+-plots: 
+    -raw data (before/after smoothing)
+    -velocity
+    -position
---
+++ walk.py
@@ -0,0 +1,38 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from pykalman import KalmanFilter
+
+
+data = pd.read_csv('data/left_leg_m.csv', header=0, names=['time', 'x', 'y', 'z', 't'])
+data = data.drop(columns=['t'])
+print(data)
+
+# loess smoothing
+lowess = sm.nonparametric.lowess
+loess_smoothed = lowess(data['x'], data['time'], frac=0.001)
+
+# kalman data filtering
+kalman_data = data[['x', 'y', 'z']]
+initial_state = kalman_data.iloc[0]
+observation_covariance = np.diag([0.98, 0.88, 0.83]) ** 2 
+transition_covariance = np.diag([0.005, 0.01, 0.01]) ** 2
+transition = [[0.97,0.5,0.2], [0.1,0.4,2.2], [0,0,0.95]] 
+
+# kalman smoothing
+kf = KalmanFilter(
+    transition_matrices=transition,
+    transition_covariance=transition_covariance,
+    observation_covariance=observation_covariance,
+    initial_state_mean=initial_state
+)
+kalman_smoothed, covariances= kf.smooth(kalman_data)
+
+plt.plot(data['time'], data['x'])
+# plt.plot(data['time'], loess_smoothed[:,1])
+plt.plot(data['time'], kalman_smoothed[:,0])
+# plt.plot(data['time'], data['y'])
+# plt.plot(data['time'], data['z'])
+plt.show()
